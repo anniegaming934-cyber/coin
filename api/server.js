112c46ce4613ce96a2553435bf1ceeafa146bc83
saminkc999 <sammykc03@gmail.com>
2025-11-19 08:45:47 +0545

    Update on project

--- api/server.js
+++
@@ -1,9 +0,0 @@
-// // server.js
-// import "dotenv/config"; // loads .env
-// import app from "./api/index.js";
-
-// const PORT = process.env.PORT || 5000;
-
-// app.listen(PORT, () => {
-//   console.log(`✅ Local API running at http://localhost:${PORT}`);
-// });
--- api/utils/numbers.js
+++
@@ -1,7 +0,0 @@
-// api/utils/numbers.js
-export const validMethods = ["cashapp", "paypal", "chime"];
-
-export const safeNum = (v) => {
-  const n = Number(v);
-  return Number.isFinite(n) ? n : 0;
-};
